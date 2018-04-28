3ee1a11d9e6225a38086c005f46f3d1836c3eebb
Gledson Rodrigues <ggledson@gmail.com>
2018-04-27 22:10:02 -0300

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -37,8 +37,9 @@ namespace xadrez_console {
                         Console.ReadLine();
                     }
                 }
-
-                Tela.imprimirTabuleiro(partida.tab);
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+                //Tela.imprimirTabuleiro(partida.tab);
             }
             catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
